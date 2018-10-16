5bb3b5f6d3a6aa1e18ff5ff7d8849d03f78b8925
Duarte Oliveira <Duda8Filipe@gmail.com>
2018-10-16 15:19:34 +0100

    Board display try n.1

--- main.pl
+++ main.pl
@@ -1,3 +1,25 @@
-b_display([X]) :- write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,
- write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,
- write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl.
+board_display([L1 | L]):- board_display([L1 | L], 10).
+
+board_display([L1 | []]], nLines) :-
+  line_display(L1, 10),
+  nLine is 11-nLines,
+  write(nLine).
+
+board_display([L1 | L], nLines) :-
+  line_display(L1, 10),
+  nLine is 11-nLines,
+  write(nLines),
+  newNLines is nLines-1,
+  board_display(newNLines).
+
+line_display([C1 | []], nCols):-
+  write(C1),
+  write(' '),
+  write(nCols),
+  nl.
+
+line_display([C1 | C], nCols):-
+  write(C1),
+  write(' '),
+  newNCols is nCols-1,
+  line_display(newNCols).
