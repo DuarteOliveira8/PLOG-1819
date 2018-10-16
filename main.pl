560541dbb23ddc47927d2ccf6ca6c1405fc58cf2
Pedrocssg <pedrokraktuga@gmail.com>
2018-10-16 11:00:57 +0100

    Main file added.

---
+++ main.pl
@@ -0,0 +1,3 @@
+b_display([X]) :- write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,
+ write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,
+ write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl,write([X,X,X,X,X,X,X,X,X,X]),nl.
