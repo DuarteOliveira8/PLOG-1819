3f481c9ae597c97c3a7c917e787f6330acfe8961
Duarte Oliveira <Duda8Filipe@gmail.com>
2018-10-19 09:17:00 +0100

    Player turn included in board display

--- main.pl
+++ main.pl
@@ -1,11 +1,13 @@
-board_display([L1]) :-
-  line_display(L1),
-  nl.
-
-board_display([L1 | L]) :-
+board_display([L1], Player) :-
   line_display(L1),
   nl,
-  board_display(L).
+  write(Player),
+  write(' turn').
+
+board_display([L1 | L], Player) :-
+  line_display(L1),
+  nl,
+  board_display(L, Player).
 
 line_display([C1]):-
   write(C1),
