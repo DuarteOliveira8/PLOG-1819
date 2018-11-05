9b8023a3be8857b63ceaabd6e17f65f6d4994070
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2018-11-05 18:38:18 +0000

    display numbers to chars

--- main.pl
+++ main.pl
@@ -1,13 +1,12 @@
+numToChar(1, C):-C='Y'.
+numToChar(2, C):-C='M'.
+numToChar(0, C):-C='O'.
+numToChar(3, C):-C='+'.
+
 board_display([L1 | L], Player) :-
   board_display([L1 | L], Player, 1).
 
-board_display([L1], Player, NLine) :-
-  write('-----------------------------------------'),
-  nl,
-  line_display(L1),
-  write(' '),
-  write(NLine),
-  nl,
+board_display([], Player, 11) :-
   write('-----------------------------------------'),
   nl,
   write('  1   2   3   4   5   6   7   8   9   10'),
@@ -25,14 +24,12 @@ board_display([L1 | L], Player, NLine) :-
   NewNLine is NLine+1,
   board_display(L, Player, NewNLine).
 
-line_display([C1]):-
-  write('| '),
-  write(C1),
-  write(' '),
+line_display([]):-
   write('|').
 
 line_display([C1 | C]):-
+  numToChar(C1, Symbol),
   write('| '),
-  write(C1),
+  write(Symbol),
   write(' '),
   line_display(C).
