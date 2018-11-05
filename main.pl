1979dd5c7447b8c368a86e4f48a082d7ce1040a0
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2018-11-05 15:15:30 +0000

    Board display enhanced

--- main.pl
+++ main.pl
@@ -1,19 +1,38 @@
-board_display([L1], Player) :-
+board_display([L1 | L], Player) :-
+  board_display([L1 | L], Player, 1).
+
+board_display([L1], Player, NLine) :-
+  write('-----------------------------------------'),
+  nl,
   line_display(L1),
+  write(' '),
+  write(NLine),
+  nl,
+  write('-----------------------------------------'),
+  nl,
+  write('  1   2   3   4   5   6   7   8   9   10'),
   nl,
   write(Player),
   write(' turn').
 
-board_display([L1 | L], Player) :-
-  line_display(L1),
+board_display([L1 | L], Player, NLine) :-
+  write('-----------------------------------------'),
   nl,
-  board_display(L, Player).
+  line_display(L1),
+  write(' '),
+  write(NLine),
+  nl,
+  NewNLine is NLine+1,
+  board_display(L, Player, NewNLine).
 
 line_display([C1]):-
+  write('| '),
   write(C1),
-  write(' ').
+  write(' '),
+  write('|').
 
 line_display([C1 | C]):-
+  write('| '),
   write(C1),
   write(' '),
   line_display(C).
