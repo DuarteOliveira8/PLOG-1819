31f2fad10b6fa455aa0225087ece00527e644c4d
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2018-11-08 18:52:12 +0000

    Game cycle and input function

--- Project1/source/game.pl
+++ Project1/source/game.pl
@@ -15,34 +15,34 @@ initializeGame(Yuki, Mina) :-
   board_display(YukiBoard, 'Mina'),
   askMinaInput(MinaX, MinaY, YukiBoard, MinaBoard),
   write('\33\[2J'),
-  board_display(MinaBoard, 'Yuki').
+  gameCycle(Yuki, YukiX, YukiY, Mina, MinaX, MinaY, MinaBoard).
 
-gameCycle(Yuki, YukiX, YukiY, Mina, Board) :-
+gameCycle(Yuki, YukiX, YukiY, Mina, MinaX, MinaY, Board) :-
   board_display(Board, 'Yuki'),
-  eatTree(YukiX, YukiY, Board),
-  askYukiInput(NewYukiX, NewYukiY, Board),
-  gameCycle(Yuki, NewYukiX, NewYukiY, Mina, Board).
+  eatTree(YukiX, YukiY, Board, NoTreeBoard),
+  askYukiInput(NewYukiX, NewYukiY, NoTreeBoard, YukiBoard),
+  board_display(YukiBoard, 'Mina'),
+  askMinaInput(NewMinaX, NewMinaY, YukiBoard, MinaBoard),
+  gameCycle(Yuki, NewYukiX, NewYukiY, Mina, NewMinaX, NewMinaY, MinaBoard).
 
 eatTree(X, Y, Board, NBoard) :-
   replaceMultList(0, X, Y, Board, NBoard).
 
 askYukiInput(X, Y, Board, NBoard) :-
-  write('Type X coordinate: '),
-  read(X),
-  nl,
-  write('Type Y coordinate: '),
-  read(Y),
-  nl,
+  askPosition(X, Y),
   changePlayerPosition(1, X, Y, Board, NBoard).
 
 askMinaInput(X, Y, Board, NBoard) :-
+  askPosition(X, Y),
+  changePlayerPosition(2, X, Y, Board, NBoard).
+
+askPosition(X, Y) :-
   write('Type X coordinate: '),
   read(X),
   nl,
   write('Type Y coordinate: '),
   read(Y),
-  nl,
-  changePlayerPosition(2, X, Y, Board, NBoard).
+  nl.
 
 changePlayerPosition(Player, X, Y, Board, NBoard) :-
   replaceMultList(Player, X, Y, Board, NBoard).
