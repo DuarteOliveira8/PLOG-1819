e2896d79e5411bf02146761b80dd093da7e643af
Pedro Gonçalves <pedrokraktuga@gmail.com>
2018-11-19 00:36:01 +0000

    Update aux.pl

--- Project1/source/aux.pl
+++ Project1/source/aux.pl
@@ -104,6 +104,7 @@ calcDistance(V1, X2, Y2, D) :-
   X2Y2 is XP2+YP2,
   D is sqrt(X2Y2).
 
+/* Gets all indexes of element X from given list */
 getIndexesOf(_X, _Index, [], []).
 
 getIndexesOf(X, Index, [L1 | L], Indexes) :-
