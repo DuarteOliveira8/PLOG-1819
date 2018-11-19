3363fdfe78ac25d1d18770a72b10decec5f2bdd1
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2018-11-19 15:53:24 +0000

    Game logic documentation

--- Project1/source/logic.pl
+++ Project1/source/logic.pl
@@ -1,24 +1,31 @@
+/* Checks if the game is over by verifying if there's any valid play left. */
 game_over(Player, _Board, Winner, ValidPlays) :-
   checkValidPlays(ValidPlays),
   Winner = Player.
 
+/* Checks if the game is over by verifying if the total sum of the board is equal to 93, which means that 70 trees were consumed. */
 game_over(Player, Board, Winner, _ValidPlays) :-
   checkTotal(Board, Value),
   Value == 93,
   Winner = Player.
 
+/* Checks if the number of valid plays is equal to 0. */
 checkValidPlays(ValidPlays) :-
   length(ValidPlays, 0).
 
+/* Calculates the total sum of the board. */
 checkTotal(Board, Value) :-
   sumMultList(0, Value, Board).
 
+/* Computes the valid moves for Yuki considering the Board state. */
 valid_moves(Board, 'Yuki', ValidPlays) :-
   findall([X,Y], checkValidYukiPlay(X, Y, Board), ValidPlays).
 
+/* Computes the valid moves for Mina considering the Board state. */
 valid_moves(Board, 'Mina', ValidPlays) :-
   findall([X,Y], checkValidMinaPlay(X, Y, Board), ValidPlays).
 
+/* Checks if X and Y are valid coordinates for Yuki's next play considering the Board state. Checks if X and Y are in the board's limits, checks if Yuki doesn't move more than one block, checks if Mina, blank space or himself are not the new position and if Mina is visible from the new position. */
 checkValidYukiPlay(X, Y, Board) :-
   between(1, 10, X),
   between(1, 10, Y),
@@ -34,7 +41,7 @@ checkValidYukiPlay(X, Y, Board) :-
   getMinaPosition(MinaX, MinaY, Board),
   isVisible(MinaX, MinaY, X, Y, Board).
 
-/* remove ifs */
+/* Checks if X and Y are valid coordinates for Mina's next play considering the Board state. Checks if X and Y are in the board's limits, checks if Mina moves orthogonally or diagonally, checks if Yuki or herself are not the new position and if Mina is visible from the new position. */
 checkValidMinaPlay(X, Y, Board) :-
   between(1, 10, X),
   between(1, 10, Y),
@@ -48,6 +55,7 @@ checkValidMinaPlay(X, Y, Board) :-
   getYukiPosition(YukiX, YukiY, Board),
   \+ isVisible(X, Y, YukiX, YukiY, Board).
 
+/* Checks if Mina moves either orthogonally or diagonally. */
 isOrtogonalOrDiagonal(DX, DY) :-
   DX > 0,
   DY == 0.
@@ -60,6 +68,7 @@ isOrtogonalOrDiagonal(DX, DY) :-
   DX == DY,
   DX =\= 0.
 
+/* Checks if Mina is visible by Yuki given X and Y coordinates for both players. */
 isVisible(MinaX, MinaY, YukiX, YukiY, Board) :-
   DX is MinaX-YukiX,
   DY is MinaY-YukiY,
@@ -67,6 +76,7 @@ isVisible(MinaX, MinaY, YukiX, YukiY, Board) :-
   calcXYratios(DX, DY, 1, RXY, RYX),
   isDirectlyVisible(GCD, DX, DY, RXY, RYX, MinaX, MinaY, YukiX, YukiY, Board).
 
+/* Computes DX and DY ratios. */
 calcXYratios(DX, 0, _Mult, RXY, RYX) :-
   DX < 0,
   RXY is -1,
@@ -119,8 +129,10 @@ calcXYratios(DX, DY, Mult, RXY, RYX) :-
   RXY is RXY1,
   RYX is RYX1.
 
+/* Mina is directly visible by Yuki. */
 isDirectlyVisible(1, _DX, _DY, _RXY, _RYX, _MinaX, _MinaY, _YukiX, _YukiY, _Board).
 
+/* Mina is not directly visible by Yuki. Computes next coords and checks if she's undirectly visible. */
 isDirectlyVisible(GCD, DX, DY, RXY, RYX, MinaX, MinaY, YukiX, YukiY, Board) :-
   GCD =\= 1,
   CRXY is ceiling(abs(RXY))*integer(sign(RXY)),
@@ -129,14 +141,18 @@ isDirectlyVisible(GCD, DX, DY, RXY, RYX, MinaX, MinaY, YukiX, YukiY, Board) :-
   value(Value, NewMinaX, NewMinaY, Board),
   isUndirectlyVisible(Value, NewMinaX, NewMinaY, YukiX, YukiY, Board).
 
+/* Mina is not visible by Yuki due to a tree blocking the line of sight. */
 isUndirectlyVisible(3, _NewMinaX, _NewMinaY, _YukiX, _YukiY, _Board) :-
   fail.
 
+/* Mina is visible by Yuki. */
 isUndirectlyVisible(1, _NewMinaX, _NewMinaY, _YukiX, _YukiY, _Board).
 
+/* No conclusions can be made about Mina's visibility since a blank space (0) was found. Checks visibility for next coordinates. */
 isUndirectlyVisible(0, NewMinaX, NewMinaY, YukiX, YukiY, Board) :-
   isVisible(NewMinaX, NewMinaY, YukiX, YukiY, Board).
 
+/* Computes next Mina coordinates for next cell in the line of sight. */
 calcNextMinaCoords(DX, DY, MinaX, MinaY, CRXY, CRYX, NewMinaX, NewMinaY) :-
   DX < 0, DY < 0,
   NewMinaX is MinaX+CRXY,
@@ -165,18 +181,21 @@ calcNextMinaCoords(_DX, DY, MinaX, _MinaY, CRXY, _CRYX, NewMinaX, _NewMinaY) :-
   DY == 0,
   NewMinaX is MinaX-CRXY.
 
+/* Updates game score according to the winner of game 1. */
 updateGame1Score(Score, 'Yuki', NewScore) :-
   addToListCell(1, 1, Score, NewScore).
 
 updateGame1Score(Score, 'Mina', NewScore) :-
   addToListCell(1, 2, Score, NewScore).
 
+/* Updates game score according to the winner of game 2. */
 updateGame2Score(Score, 'Yuki', NewScore) :-
   addToListCell(1, 2, Score, NewScore).
 
 updateGame2Score(Score, 'Mina', NewScore) :-
   addToListCell(1, 1, Score, NewScore).
 
+/* Announces the winner of the set (2 games). */
 announceSetWinner(Score, _CharWinner, _Game1Board, _Game2Board) :-
   checkValueList(2, 1, Score),
   write('Player 1 wins with both characters! He\'s the hide and seek master!').
@@ -207,6 +226,7 @@ announceSetWinner(Score, 'Mina', G1B, G2B) :-
   NumTreesG2 is (300-TotalG2-3)/3,
   getFinalWinner('Mina', NumTreesG1, NumTreesG2).
 
+/* Displays the winner message in case of game draw. In this case winner is based on trees consumed by Yuki. */
 getFinalWinner('Yuki', NumTreesG1, NumTreesG2) :-
   NumTreesG1 < NumTreesG2,
   write('Both players won as Yuki.\n'),
